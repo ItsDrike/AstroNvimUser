6bbb3bfad40f0e0db7b6ce29aff7984781471ad3
ItsDrike <itsdrike@protonmail.com>
2023-04-11 18:53:57 +0200

    Add wakatime plugin

---
+++ plugins/wakatime.lua
@@ -0,0 +1,20 @@
+-- TODO: Wakatime
+return {
+  {
+    -- Code time & habit tracking
+    "wakatime/vim-wakatime",
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = {
+      "WakaTimeApiKey",
+      "WakaTimeDebugEnable",
+      "WakaTimeDebugDisable",
+      "WakaTimeScreenRedrawEnable",
+      "WakaTimeScreenRedrawEnableAuto",
+      "WakaTimeScreenRedrawDisable",
+      "WakaTimeToday",
+      "WakaTimeCliLocation",
+      "WakaTimeCliVersion",
+      "WakaTimeFileExpert",
+    },
+  },
+}
