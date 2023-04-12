ef5102f94e877a4f1fb70a95f5c219ca3e72df6b
ItsDrike <itsdrike@protonmail.com>
2023-04-12 19:03:25 +0200

    Add LSP autoformatting settings

---
+++ lsp/formatting.lua
@@ -0,0 +1,10 @@
+return {
+  enabled = true,
+  ignore_filetypes = {
+    "markdown",
+    "python",
+  },
+  --allow_filetypes = { "lua" },
+  --disabled = { "sumenko_lua", "rust_analyzer" },
+  --timeout_ms = 3200
+}
