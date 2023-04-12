9016429b59588cacc41baa2a93502fc3ae2542ea
ItsDrike <itsdrike@protonmail.com>
2023-04-12 18:57:18 +0200

    Add keybind to toggle LSP diagnostics

--- lsp/on_attach.lua
+++ lsp/on_attach.lua
@@ -1,4 +1,14 @@
 return function(client, bufnr)
+  local diagnostics_active = true
+  vim.keymap.set("n", "<leader>lt", function()
+    diagnostics_active = not diagnostics_active
+    if diagnostics_active then
+      vim.diagnostic.show()
+    else
+      vim.diagnostic.hide()
+    end
+  end, { buffer = bufnr, desc = "Toggle diagnostics" })
+
   if client.server_capabilities.inlayHintProvider then
     local inlayhints_avail, inlayhints = pcall(require, "lsp-inlayhints")
     if inlayhints_avail then
