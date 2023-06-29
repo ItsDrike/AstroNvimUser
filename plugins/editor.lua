55612d109f55ca6fb545427e8207ad12f4875fef
ItsDrike <itsdrike@protonmail.com>
2023-06-30 01:48:36 +0200

    Add headlines.nvim

--- plugins/editor.lua
+++ plugins/editor.lua
@@ -382,4 +382,10 @@ return {
       }
     end,
   },
+  {
+    "lukas-reineke/headlines.nvim",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    ft = { "markdown" },
+    opts = {},
+  },
 }
