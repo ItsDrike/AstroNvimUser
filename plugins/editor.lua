0f7c9230df3bb36568f0c5b3191a4b3576af2f88
ItsDrike <itsdrike@protonmail.com>
2023-08-07 23:35:03 +0200

    Follow current file in neotree

--- plugins/editor.lua
+++ plugins/editor.lua
@@ -4,6 +4,7 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
       filesystem = {
+        follow_current_file = true,
         hijack_netrw_behavior = "open_default",
         filtered_items = {
           always_show = {
