1c88bc8e36ecb4f0fe2cce95cb29b9d7c7d55fea
ItsDrike <itsdrike@protonmail.com>
2024-04-03 18:26:25 +0200

    Enable neoconf

---
+++ lua/plugins/neoconf-nvim.lua
@@ -0,0 +1,7 @@
+-- Enable neoconf
+--
+-- Neoconf is supported in AstroNvim, however only as an optional dependnecy
+return {
+  "folke/neoconf.nvim",
+  cmd = "NeoConf", -- Also add a command invocation support for pre-load
+}
