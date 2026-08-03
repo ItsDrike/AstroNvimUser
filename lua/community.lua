d57291e74ae73d9ad6c8a116b8054f6e028b6d80
ItsDrike <itsdrike@protonmail.com>
2026-08-03 13:15:17 +0200

    Add some more lang astrocommunity packs

--- lua/community.lua
+++ lua/community.lua
@@ -8,17 +8,20 @@ return {
 
   -- Languages
   { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.sql" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.terraform" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.python.base" },
   { import = "astrocommunity.pack.python.basedpyright" },
   --{ import = "astrocommunity.pack.python.ty" },
   { import = "astrocommunity.pack.python.ruff" },
+  { import = "astrocommunity.pack.rust" },
   {
     import = "astrocommunity.pack.rust",
     dependencies = {
