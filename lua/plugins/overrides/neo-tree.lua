4e8828f84ccb048825bf6a45447e4a06dbd8ff4c
ItsDrike <itsdrike@protonmail.com>
2026-08-03 10:44:46 +0200

    Decrease neo-tree indent size

---
+++ lua/plugins/overrides/neo-tree.lua
@@ -0,0 +1,19 @@
+---@type LazySpec
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    optional = true,
+    opts = {
+      -- Use a smaller indent size without padding
+      -- (works better when working with projects that do a lot of dir nesting)
+      default_component_configs = {
+        indent = {
+          indent_size = 2,
+          padding = 0,
+          with_markers = true,
+          with_expanders = true,
+        },
+      },
+    },
+  },
+}
