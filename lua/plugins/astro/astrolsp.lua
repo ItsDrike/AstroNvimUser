5249aeb99e0e95cb0a41af5e342ce0c5452ede79
ItsDrike <itsdrike@protonmail.com>
2026-08-03 10:43:08 +0200

    Only auto-format allowed types

--- lua/plugins/astro/astrolsp.lua
+++ lua/plugins/astro/astrolsp.lua
@@ -18,11 +18,10 @@ return {
       format_on_save = {
         enabled = true, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
-          -- "go",
+          "rust",
+          "python",
         },
-        ignore_filetypes = { -- disable format on save for specified filetypes
-          -- "python",
-        },
+        ignore_filetypes = {},
       },
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
