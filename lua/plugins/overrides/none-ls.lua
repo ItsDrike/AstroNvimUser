41dffc407bdc41ec15db7967251a7d34a81f51bb
ItsDrike <itsdrike@protonmail.com>
2026-09-15 16:43:53 +0200

    Use postgres dialect for sqlfluff

--- lua/plugins/overrides/none-ls.lua
+++ lua/plugins/overrides/none-ls.lua
@@ -6,5 +6,18 @@ return {
       -- see: https://github.com/AstroNvim/astrocommunity/issues/1794
       autocmds = { sqls_attach = false },
     },
-  }
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.handlers = opts.handlers or {}
+      local null_ls = require "null-ls"
+      local sqlfluff = { extra_args = { "--dialect", "postgres" } }
+      opts.handlers.sqlfluff = function()
+        null_ls.register(null_ls.builtins.diagnostics.sqlfluff.with(sqlfluff))
+        null_ls.register(null_ls.builtins.formatting.sqlfluff.with(sqlfluff))
+      end
+    end,
+  },
 }
